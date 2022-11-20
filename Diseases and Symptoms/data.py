bebd03491025bc28124f009266a6f24f66671469
Bill Bai <31229051+billbai0102@users.noreply.github.com>
2022-11-19 22:04:06 -0500

    data

---
+++ Diseases and Symptoms/data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import cohere
+
+descriptions = pd.read_csv("./archive/symptom_Description.csv")
+
+co = cohere.Client("JuqpapPUIT9dRAH5a06D4rIj7tTnbWVwY59dY4eC")
+texts = descriptions['Description'].tolist()
+response = co.embed(
+    model='large',
+    texts=texts)
+
+name = descriptions['Disease'].tolist()
+desc = descriptions['Description'].tolist()
+embeds = response.embeddings
+
+for i in range(len(name)):
+    print("name: ", name[i])
+    print("desc: ", desc[i])
+    print("embed: ", embeds[i])
+    print()
